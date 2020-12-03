5b2c186c13b7d56e96951bea82390c857725d898
Nathan Howell <nathan.d.howell@gmail.com>
2020-12-03 09:01:25 -0800

    more wonking

--- src/Day3.hs
+++ src/Day3.hs
@@ -3,45 +3,26 @@ module Day3 (
   problem3Part2,
 ) where
 
-isTree :: Char -> Bool
-isTree = (==) '#'
+import Control.Monad
 
-parseMapRow :: String -> [Bool]
-parseMapRow = map isTree
+isTree :: Char -> Int
+isTree c
+  | c == '#' = 1
+  | otherwise = 0
 
-countTrees :: Int -> [[Bool]] -> Int
-countTrees step treeMap =
-  countTreesCols step 0 treeMap
-  where
-    countTreesCols :: Int -> Int -> [[Bool]] -> Int
-    countTreesCols _ _ [] = 0
-    countTreesCols slope col (row:rows) =
-      (+) tree $ countTreesCols slope (col + slope) rows
-      where
-        tree =
-          if row !! (mod col (length row))
-          then 1
-          else 0
+parseMapRow :: String -> [Int]
+parseMapRow = cycle . map isTree
+
+countTrees :: Int -> Int -> [[Int]] -> Int
+countTrees right down treeMap = sum $ do
+  (row, idx) <- zip treeMap [0..]
+  guard $ idx `mod` down == 0
+  return $! row !! (idx*right)
 
 problem3 :: [String] -> Int
-problem3 = countTrees 3 . map parseMapRow
+problem3 = countTrees 3 1 . map parseMapRow
 
 problem3Part2 :: [String] -> Int
-problem3Part2 rows =
-  twoRows * (product $ map (\ f -> f treeMap) $ map countTrees paths)
-  where
-    treeMap = map parseMapRow rows
-    paths = [
-        1,
-        3,
-        5,
-        7
-      ]
-
-    twoRows = countTrees 1 $ skipRows treeMap
-
-    skipRows :: [a] -> [a]
-    skipRows [] = []
-    skipRows (row:[]) = []
-    skipRows (row : _: []) = [row]
-    skipRows (row:_:rows) = row:(skipRows rows)
+problem3Part2 rows = sum [countTrees right down treeMap | (right, down) <- slopes] where
+  treeMap = map parseMapRow rows
+  slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
