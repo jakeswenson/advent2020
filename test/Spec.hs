9575340f7cc4499625d7b1a5250ac9afdc5821f0
Jake Swenson <jake@jakeswenson.com>
2020-12-03 21:14:47 -0800

    work on problem 4

--- test/Spec.hs
+++ test/Spec.hs
@@ -76,3 +76,27 @@ day03 = testGroup "Problem 03" [
   testCase "Part 2" $ do
     Day3.part2 exampleDay3 @?= 336
   ]
+
+
+exampleDay4 = unlines [
+  "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\
+   \byr:1937 iyr:2017 cid:147 hgt:183cm",
+  "",
+   "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\
+   \hcl:#cfa07d byr:1929",
+   "",
+   "hcl:#ae17e1 iyr:2013\
+   \eyr:2024\
+   \ecl:brn pid:760753108 byr:1931\
+   \hgt:179cm",
+  "",
+   "hcl:#cfa07d eyr:2025 pid:166559648\
+   \iyr:2011 ecl:brn hgt:59in"
+  ]
+
+day04 = testGroup "Problem 04" [
+  testCase "Example" $ do
+    Day3.part1 exampleDay3 @?= 7,
+  testCase "Part 2" $ do
+    Day3.part2 exampleDay3 @?= 336
+  ]
