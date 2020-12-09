7cfd902c20ab172a6bc9253a11257bd4cb9be40c
Jake Swenson <jake@jakeswenson.com>
2020-12-09 08:19:09 -0800

    Day 8

--- src/Day8.hs
+++ src/Day8.hs
@@ -4,30 +4,123 @@
 module Day8 (
   part1,
   part2,
+  parseInstruction,
+  Instruction(..),
+  Marked(..),
+  jump
 ) where
 
 
 import Control.Monad
 import Data.Maybe (mapMaybe)
 import Data.Monoid (Sum(..))
-import Data.Either(fromRight)
+import Data.Either(fromRight, rights)
 import Data.Either.Combinators (rightToMaybe)
 import qualified Data.Text as T
 import Text.Megaparsec
 import Text.Megaparsec.Char (char, lowerChar, space, space1, string)
-import Text.Megaparsec.Char.Lexer (decimal)
+import Text.Megaparsec.Char.Lexer (signed, decimal)
 import Data.Void
 import Data.Char(isAlpha)
 
+type Parser = Parsec Void T.Text
+
 data Instruction =
-  Accumulator Int
+  Accumulator Integer
   | Jump Int
-  | NoOp
+  | NoOp Int
+  deriving (Show, Eq, Ord)
 
-type Program = [Instruction]
+data Marked = NotRun Instruction | Ran Instruction
+  deriving (Show, Eq, Ord)
 
-part1 :: String -> Int
-part1 = length
+parseInstruction :: Parser Instruction
+parseInstruction =
+    acc <|> jmp <|> nop
+  where
+    acc = do
+      string "acc"
+      space1
+      adjustment <- signed space decimal
+      return $ Accumulator adjustment
+    jmp = do
+      string "jmp"
+      space1
+      offset <- fromInteger <$> signed space decimal
+      return $ Jump offset
+    nop = do
+      string "nop"
+      space1
+      value <- signed space decimal
+      return $ NoOp value
 
-part2 :: String -> Int
-part2 = length
+jump :: Int -> ([Marked], [Marked]) -> ([Marked], [Marked])
+jump o (_, []) | o < 0 = error ("Can't go back without history" ++ show o)
+jump o ([], _) | o > 0 = error ("Can't go forward without next: " ++ show o)
+jump 0 (next, history) = ((Ran $ Jump 0):next, history)
+jump offset (next, history) | offset < 0 =
+  doJumpBack offset (inst:next, history)
+  where
+    inst = Ran $ Jump offset
+    doJumpBack 0 a = a
+    doJumpBack offset (next, ran:history) =
+      doJumpBack (offset + 1) (ran:next, history)
+
+jump offset (next, history) | offset > 0 =
+  doJump offset (inst:next, history)
+  where
+    inst = Ran $ Jump offset
+    doJump 0 a = a
+    doJump offset (current:next, history) =
+      doJump (offset-1) (next, current:history)
+
+type InterpretResult = Integer
+
+runOnce :: Integer -> [Marked] -> [Marked] -> InterpretResult
+runOnce a [] _ = a
+runOnce acc (current:rest) history =
+  case current of
+    Ran dupe -> acc
+    NotRun inst -> case inst of
+        Accumulator adjustment ->
+          runOnce (acc + adjustment) rest ((Ran inst):history)
+        Jump offset ->
+          uncurry (runOnce acc) $ jump offset (rest, history)
+        NoOp _ -> runOnce acc rest ((Ran inst):history)
+
+interpret :: [Instruction] -> InterpretResult
+interpret instructions =
+  runOnce 0 (markNotRun instructions) []
+  where
+    markNotRun = map NotRun
+
+part1 :: String -> Integer
+part1 =
+  interpret . instructions
+  where
+    instructions = rights . map (parse parseInstruction "line") . T.lines . T.pack
+
+fix :: Bool -> Integer -> [Marked] -> [Marked] -> Maybe InterpretResult
+fix _ a [] _ = Just a
+fix hasReplaced acc (current:rest) history =
+  case current of
+    Ran dupe -> Nothing
+    NotRun inst -> case inst of
+        Accumulator adjustment ->
+          fix hasReplaced (acc + adjustment) rest ((Ran inst):history)
+        Jump offset -> -- backwards
+          case uncurry (fix hasReplaced  acc) $ jump offset (rest, history) of
+            Nothing | not hasReplaced -> fix True acc ((NotRun $ NoOp offset):rest) history -- Try NoOp
+            a -> a
+        NoOp offset -> case fix hasReplaced acc rest ((Ran inst):history) of
+          Nothing | not hasReplaced -> fix True acc ((NotRun $ Jump offset):rest) history
+          a -> a
+
+
+part2 :: String -> Maybe Integer
+part2 input =
+  findFixVal $ (instructions input) ++ [Accumulator 0]
+  where
+    findFixVal :: [Instruction] -> Maybe Integer
+    findFixVal insts = fix False 0 (map NotRun insts) []
+    instructions = rights  . map (parse parseInstruction "line") . T.lines . T.pack
--- test/Days/Day08.hs
+++ test/Days/Day08.hs
@@ -1,8 +1,13 @@
+{-# language OverloadedStrings #-}
+
 module Days.Day08(tests) where
+
+import Text.Megaparsec(parseTest, parse, errorBundlePretty)
+
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import Day8(part1, part2)
+import Day8(part1, part2, parseInstruction, Instruction(..), jump, Marked(..))
 
 example = unlines [
     "nop +0",
@@ -16,9 +21,64 @@ example = unlines [
     "acc +6"
   ]
 
+parseIt parser input =
+  case parse parser "test" input of
+    Left bundle -> error (errorBundlePretty bundle)
+    Right r -> r
+
+
+nop = NotRun $ NoOp 0
+acc :: Integer -> Marked
+acc = NotRun . Accumulator
+jmp = NotRun . Jump
+
+ran :: Marked -> Marked
+ran (NotRun inst) = Ran inst
+ran i = i
+
 tests = testGroup "Problem 08" [
+  testGroup "Parser" [
+    testCase "acc" $ do
+      parseIt parseInstruction "acc -4" @?= Accumulator (-4),
+    testCase "jmp" $ do
+      parseIt parseInstruction "jmp -4" @?= Jump (-4),
+    testCase "nop" $ do
+      parseIt parseInstruction "nop +7" @?= NoOp 7
+  ],
+  testGroup "Interpreter" [
+    testCase "Simple: jumpForwards" $ do
+      jump 1 ([ acc 3, nop ], []) @?= ([acc 3, nop], [ ran $ jmp 1]),
+
+    testCase "Simple: jumpBackwards" $ do
+      jump (-1) ([ ], [nop, acc 3]) @?= ([nop, ran $ jmp (-1)], [acc 3]),
+
+    testCase "jumpForwards" $ do
+      jump 2 ([ acc 3, nop ], []) @?= ([nop], [acc 3, ran $ jmp 2]),
+
+    testCase "jumpBackwards" $ do
+      jump (-2) ([ ], [nop, acc 3]) @?= ([acc 3, nop, ran $ jmp (-2)], []),
+
+    testCase "Example: jumpForwards" $ do
+      jump 4 ([
+          acc 3,
+          jmp (-3),
+          acc (-99),
+          acc 1
+        ], [  acc 1, nop ]) @?= ([
+          acc 1
+        ],
+        [
+          acc (-99),
+          jmp (-3),
+          acc 3,
+          ran$ jmp 4,
+          acc 1,
+          nop
+        ])
+
+  ],
   testCase "Example" $ do
     part1 example @?= 5,
   testCase "Part 2" $ do
-    part2 example @?= 6
+    part2 example @?= Just 8
   ]
